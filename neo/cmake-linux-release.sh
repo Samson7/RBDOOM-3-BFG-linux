958b10c91e86f7c347c86d7f45d393d4ad0a9093
Samson7 <sams.777@yandex.ru>
2024-11-01 14:47:40 +0300

    Update cmake-linux-release.sh

--- neo/cmake-linux-release.sh
+++ neo/cmake-linux-release.sh
@@ -4,4 +4,6 @@ cd ..
 rm -rf build
 mkdir build
 cd build
+add_definitions(-Wno-format-zero-length)
+add_definitions(-Wno-nonnull)
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DONATIVE=ON -DFFMPEG=OFF -DBINKDEC=ON ../neo
